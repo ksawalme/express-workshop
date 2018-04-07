98f45df2a67c65644583f484f9e65f7e417c0b44
ksawalme <ksawalme@gmail.com>
2018-04-07 15:59:58 +0100

    Step 5 - Routing

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", function(req, res) {
-res.send("hey lorenzo thenk you very much i am rely interest");
-});
-
+app.use(express.static("public"));
+    
 
 app.listen(3000, function() {
     console.log("Server is listening on port 3000. Ready to accept requests!");
