31b232ebe973ebb339f861d828f5f7747c910ec8
ksawalme <ksawalme@gmail.com>
2018-04-07 16:00:55 +0100

    Step 6 - Serving static files

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+
 const express = require("express");
 
 const app = express();
 
 app.use(express.static("public"));
-    
+
 
 app.listen(3000, function() {
     console.log("Server is listening on port 3000. Ready to accept requests!");
