f2071aee35db9eab844ede19d903c8cf368d6e9d
ksawalme <ksawalme@gmail.com>
2018-04-07 16:29:52 +0100

    the post request method

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static("public"));
+app.post("/create-post", function(req, res) {
+    console.log("I am /create-post endpoint");
+  });
+  
 
+app.use(express.static("public"));
 
 app.listen(3000, function() {
     console.log("Server is listening on port 3000. Ready to accept requests!");
