f58ff829552ed18c772ec37737316a80e2aeb188
ksawalme <ksawalme@gmail.com>
2018-04-08 22:47:10 +0100

    Extracting the blog post

--- server.js
+++ server.js
@@ -2,18 +2,20 @@
 const express = require("express");
 
 const app = express();
-
-app.post("/create-post", function(req, res) {
-    console.log("I am /create-post endpoint");
-  });
-  
-
-  app.get("/hello-world", function(req, res) {
-  res.send("Hello there!");
-});
-
+const formidable = require("express-formidable");
 
 app.use(express.static("public"));
+
+
+
+app.post("/create-post", function(req, res) {
+    console.log(req.fields);
+});
+
+app.use(formidable());
+
+
+
 
 app.listen(3000, function() {
     console.log("Server is listening on port 3000. Ready to accept requests!");
