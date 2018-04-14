7d57538aeab93dc538b710d299097c919d50f367
ksawalme <ksawalme@gmail.com>
2018-04-14 15:42:37 +0100

    save the data insid the file

--- server.js
+++ server.js
@@ -1,36 +1,47 @@
+const formidable = require("express-formidable");
+
 const fs = require("fs");
 
 const express = require("express");
 
 const app = express();
-const formidable = require("express-formidable");
 
 app.use(express.static("public"));
+
+app.use(formidable());
+
 
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 
 app.set('view engine', 'ejs');
 
-app.get('/about', function (req, res)
+app.get('/about',  function (req, res)
 {
     res.render('about.html');
 });
 
+
+
 app.post("/create-post", function(req, res) {
     console.log(req.fields);
+    fs.readFile(__dirname + "/data/posts.json", function(error, file) {
+         parsedFile = JSON.parse(file);
+         parsedFile[new Date().getTime()] = req.fields.blogpost;
+        console.log(parsedFile);
+        fs.writeFile(__dirname + "/data/posts.json",JSON.stringify(parsedFile), function(error) {
+
+        });
+    });
 
 });
 
 
+fs.writeFile(__dirname + "/data/text.json","tttt", function(error) {
 
-app.use(formidable());
-
-
-fs.readFile(__dirname + "/data/posts.json", function(error, file) {
-    console.log(file.toString());
-});
-
+    // do something
+  });
+  
 
 
 app.listen(3000, function() {
